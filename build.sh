00d497271b0e6382b10144972a2f4fc920ba671c
Harish Bohara <harish.bohara@gmail.com>
2022-10-29 13:18:09 +0530

    added build script

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 cp -a ./_install/* ~/initramfs/
 cp custom_init ~/initramfs/init
 chmod +x ~/initramfs/init
+(cd ~/initramfs && find . -print0 | cpio --null -ov --format=newc | gzip -9 > ~/initramfs.cpio.gz)
