f203738ad2796bb58748c2db4c89e048399e7a07
Harish Bohara <harish.bohara@gmail.com>
2022-10-29 13:24:33 +0530

    added build script

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
-(mkdir -p ~/initramfs && cd ~/initramfs && mkdir -p bin sbin etc proc sys usr/bin usr/sbin lib)
-cp -a ./_install/* ~/initramfs/
-cp custom_init ~/initramfs/init
-chmod +x ~/initramfs/init
+
+# Make 
+RMAFS_FOLDER=~/initramfs
+(mkdir -p $RMAFS_FOLDER && cd $RMAFS_FOLDER && mkdir -p bin sbin etc proc sys usr/bin usr/sbin lib)
+cp -a ./_install/* $RMAFS_FOLDER
+cp custom_init $RMAFS_FOLDER/init
+chmod +x $RMAFS_FOLDER/init
 
 # Build a custom command
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w -extldflags "-static"' -o fetch_google main.go
-chmod +x fetch_google && cp fetch_google ~/initramfs
+chmod +x fetch_google && cp fetch_google $RMAFS_FOLDER
 
 # Make initramfs
-(cd ~/initramfs && find . -print0 | cpio --null -ov --format=newc | gzip -9 > ~/initramfs.cpio.gz)
+(cd $RMAFS_FOLDER && find . -print0 | cpio --null -ov --format=newc | gzip -9 > ~/initramfs.cpio.gz)
