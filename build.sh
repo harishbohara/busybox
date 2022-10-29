beaae510c4acfafc06012007127f761735a8c313
Harish Bohara <harish.bohara@gmail.com>
2022-10-29 13:16:53 +0530

    added build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 (mkdir -p ~/initramfs && cd ~/initramfs && mkdir -p bin sbin etc proc sys usr/bin usr/sbin lib)
-(cd ~/initramfs && chmod +x custom_init)
+chmod +x custom_init
 cp -a ./_install/* ~/initramfs/
 cp custom_init ~/initramfs/init
