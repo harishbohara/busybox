281d52ea1708bf636748697aa5b762cec3d0bc9f
Harish Bohara <harish.bohara@gmail.com>
2022-10-29 13:21:22 +0530

    added build script

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 cp -a ./_install/* ~/initramfs/
 cp custom_init ~/initramfs/init
 chmod +x ~/initramfs/init
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w -extldflags "-static"' -o fetch_google main.go
+chmod +x fetch_google && cp call ~/initramfs
 (cd ~/initramfs && find . -print0 | cpio --null -ov --format=newc | gzip -9 > ~/initramfs.cpio.gz)
