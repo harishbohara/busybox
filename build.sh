26c0c5177d317cbc3962fe36715e2d056c96d2f7
Harish Bohara <harish.bohara@gmail.com>
2022-10-29 13:22:46 +0530

    added build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ chmod +x ~/initramfs/init
 
 # Build a custom command
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w -extldflags "-static"' -o fetch_google main.go
-chmod +x fetch_google && cp call ~/initramfs
+chmod +x fetch_google && cp fetch_google ~/initramfs
 
 # Make initramfs
 (cd ~/initramfs && find . -print0 | cpio --null -ov --format=newc | gzip -9 > ~/initramfs.cpio.gz)
